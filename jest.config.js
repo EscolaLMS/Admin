58e47e3d16e4877be323551d8ac81fe01895a98b
Mateusz <mateusz@qunabu.com>
2021-03-01 17:04:49 +0100

    docker compose

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testURL: 'http://localhost:8000',
   testEnvironment: './tests/PuppeteerEnvironment',
-  verbose: false,
+  verbose: true,
   extraSetupFiles: ['./tests/setupTests.js'],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
