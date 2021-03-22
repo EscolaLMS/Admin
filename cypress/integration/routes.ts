7332ebf45fc9b9c793609df0631c0dc11e0b948a
Mateusz Qunabu <mateusz@qunabu.com>
2021-03-22 21:39:12 +0100

    cypress e2e recording. additional tests (#6)
    * cypress e2e recording
    
    * cypress e2e recording

---
+++ cypress/integration/routes.ts
@@ -0,0 +1,43 @@
+import RouterConfig from '../../config/routes';
+const { uniq } = require('lodash');
+
+const BASE_URL = `http://localhost:${process.env.PORT || 8000}`;
+
+const getPaths = (routes) => {
+  return uniq(
+    routes
+      .map((route) => {
+        if (route.routes) {
+          return [route.component ? route.path : '', ...getPaths(route.routes)];
+        }
+        return route.component ? route.path : '';
+      })
+      .flat(100000)
+      .filter((path) => !!path),
+  );
+};
+
+describe('Routes. HomePage', () => {
+  it('HomePage', () => {
+    cy.visit(BASE_URL);
+    cy.setLocalStorage('antd-pro-authority', '["admin"]');
+    cy.get('footer').should('be.visible');
+  });
+});
+
+describe('Routes. Other', () => {
+  it('HomePage', () => {
+    cy.visit(BASE_URL);
+    cy.setLocalStorage('antd-pro-authority', '["admin"]');
+    cy.get('footer').should('be.visible');
+  });
+
+  const routers = getPaths(RouterConfig);
+  routers.forEach((route) => {
+    it(`test page ${route}`, () => {
+      cy.visit(`${BASE_URL}${route}`);
+      cy.setLocalStorage('antd-pro-authority', '["admin"]');
+      cy.get('footer').should('be.visible');
+    });
+  });
+});
