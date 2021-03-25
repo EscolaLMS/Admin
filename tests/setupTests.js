45075e917c97d07af1bde7b278e41e6ce1c38edb
Mateusz <mateusz@qunabu.com>
2021-03-25 08:39:09 +0100

    jest & codecovarage

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,35 +1,4 @@
-﻿// do some test init
-import '@testing-library/jest-dom/extend-expect';
+﻿import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-  clear: jest.fn(),
-};
-
-global.localStorage = localStorageMock;
-
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-
-/*
-window.matchMedia =
-  window.matchMedia ||
-  function (): {
-    matches: boolean,
-    addListener: () => void,
-    removeListener: () => void,
-  } {
-    return {
-      matches: false,
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      addListener: function () {},
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      removeListener: function () {},
-    };
-  };
-
-*/
+configure({ adapter: new Adapter() });
