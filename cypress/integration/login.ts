e9044e4d2f7262285b5cd36b68814766b25096bf
Mateusz <mateusz@qunabu.com>
2021-06-07 17:18:22 +0200

    fix cypress login test

--- cypress/integration/login.ts
+++ cypress/integration/login.ts
@@ -1,8 +1,11 @@
 describe('Login form', () => {
   it('Basic functionality of login form', () => {
     cy.visit('http://localhost:8000/user/login');
-    cy.get('input[id="username"]').clear().type('admin').should('have.value', 'admin');
-    cy.get('input[id="password"]').clear().type('ant.design').should('have.value', 'ant.design');
+    cy.get('input[id="email"]')
+      .clear()
+      .type('admin@escola-lms.com')
+      .should('have.value', 'admin@escola-lms.com');
+    cy.get('input[id="password"]').clear().type('secret').should('have.value', 'secret');
     cy.get('form button').click();
     cy.contains('Welcome');
   });
