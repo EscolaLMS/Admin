1009287694c58ccdce34dcea372363312cf5d135
Mateusz <mateusz@qunabu.com>
2021-08-04 15:00:01 +0200

    wysiwyg upload

--- src/components/WysiwygMarkdown/index.tsx
+++ src/components/WysiwygMarkdown/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import Editor from 'rich-markdown-editor';
-
+import { upload, resizedImage } from '@/services/escola-lms/files';
 import './index.css';
 
 interface FormWysiwygProps {
   value?: string;
   defaultValue?: string;
   onChange?: (value: string) => void;
+  directory?: string;
 }
 
-export const FormWysiwyg: React.FC<FormWysiwygProps> = ({ value, onChange }) => {
+export const FormWysiwyg: React.FC<FormWysiwygProps> = ({ value, onChange, directory = '/' }) => {
   return (
     <div className="form-wysiwyg-markdown">
       <Editor
+        uploadImage={async (file) => {
+          const result = await upload(file, directory);
+          if (result.success) {
+            return resizedImage(`${directory}${result.data[0].name}`, 1000);
+            return result.data[0].url;
+          }
+          throw new Error('Error while uploading');
+        }}
         defaultValue={value}
         onChange={(v) => onChange && onChange(v())}
         placeholder="Type here text to see the rich text editor features. Start with '/' to open formatting options or click on '+' button"
