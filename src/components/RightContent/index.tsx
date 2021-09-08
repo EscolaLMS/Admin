801cfe16befdd7131de6700dc576d7278b8a68b5
dawidzebacki <64470068+dawidzebacki@users.noreply.github.com>
2021-09-08 19:00:03 +0200

    Issue/99 (#100)
    * add: icons
    
    * add: components translations
    
    * delete: flags
    
    * add: PL and ENG translations
    
    * add: pages translations
    
    * add: translations after merge
    
    * delete: searchar and question icon
    
    * delete: HeaderSearch, intl import from RightContent component
    
    Co-authored-by: Dawid Zębacki <dawid.zebacki@escola.pl>

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,11 +1,8 @@
 import { Tag, Space } from 'antd';
-import { QuestionCircleOutlined } from '@ant-design/icons';
 import React from 'react';
 import { useModel, SelectLang } from 'umi';
 import Avatar from './AvatarDropdown';
-import HeaderSearch from '../HeaderSearch';
 import styles from './index.less';
-import { useIntl } from 'umi';
 
 export type SiderTheme = 'light' | 'dark';
 
@@ -16,7 +13,6 @@ const ENVTagColor = {
 };
 
 const GlobalHeaderRight: React.FC = () => {
-  const intl = useIntl();
   const { initialState } = useModel('@@initialState');
 
   if (!initialState || !initialState.settings) {
@@ -31,38 +27,6 @@ const GlobalHeaderRight: React.FC = () => {
   }
   return (
     <Space className={className}>
-      <HeaderSearch
-        className={`${styles.action} ${styles.search}`}
-        placeholder={intl.formatMessage({
-          id: 'search',
-        })}
-        options={[
-          { label: <a href="https://umijs.org/zh/guide/umi-ui.html">umi ui</a>, value: 'umi ui' },
-          {
-            label: <a href="next.ant.design">Ant Design</a>,
-            value: 'Ant Design',
-          },
-          {
-            label: <a href="https://protable.ant.design/">Pro Table</a>,
-            value: 'Pro Table',
-          },
-          {
-            label: <a href="https://prolayout.ant.design/">Pro Layout</a>,
-            value: 'Pro Layout',
-          },
-        ]}
-        // onSearch={value => {
-        //   console.log('input', value);
-        // }}
-      />
-      <span
-        className={styles.action}
-        onClick={() => {
-          window.open('https://pro.ant.design/docs/getting-started');
-        }}
-      >
-        <QuestionCircleOutlined />
-      </span>
       <Avatar />
       {REACT_APP_ENV && (
         <span>
