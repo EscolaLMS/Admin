bf9420a86a392dcd3d95e53859b11cb9ede94bd3
Mateusz Wojczal <mateusz@wojczal.com>
2021-09-13 18:11:17 +0200

    pages

--- src/services/escola-lms/slug.ts
+++ src/services/escola-lms/slug.ts
@@ -41,7 +41,7 @@ export const slugify = (input: string, separator = '-') => {
     .toLowerCase()
     .replace(/\s+/g, '-') // Replace spaces with -
     .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^w-]+/g, '') // Remove all non-word chars
+    .replace(/[^\w-]+/g, '') // Remove all non-word chars
     .replace(/--+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, ''); // Trim - from end of text
