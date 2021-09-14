35472afc67604308feb54eaba4a96183de7f3907
dawidzebacki <64470068+dawidzebacki@users.noreply.github.com>
2021-09-14 12:36:56 +0200

    fix: logout endpoints (#118)
    Co-authored-by: Dawid Zębacki <dawid.zebacki@escola.pl>

--- src/services/escola-lms/login.ts
+++ src/services/escola-lms/login.ts
@@ -13,3 +13,13 @@ export async function login(body: API.LoginRequest, options?: { [key: string]: a
     ...(options || {}),
   });
 }
+
+export async function logout(options?: { [key: string]: any }) {
+  return request<API.LogoutResponse>('/api/auth/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...(options || {}),
+  });
+}
