6643f95f68b26f3345c88b7658d41d4a2a1eb966
Dawid Zębacki <dawid.zebacki@escola.pl>
2021-09-15 13:31:16 +0200

    add: permissions

--- src/access.ts
+++ src/access.ts
@@ -1,9 +1,13 @@
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
-export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
-  const { currentUser } = initialState || {};
+ export default function access(initialState: { currentUser: any }) {
+  const { currentUser } = initialState;
+
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
+    adminPermission: currentUser && currentUser.data.roles.includes('admin'),
+    tutorPermission:
+      currentUser &&
+      (currentUser.data.roles.includes('tutor') || currentUser.data.roles.includes('admin')),
   };
 }
