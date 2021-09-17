8e2b2d203848db9d03b52c84d818742bceffab6a
Dawid Zębacki <dawid.zebacki@escola.pl>
2021-09-17 13:35:59 +0200

    add: translations

--- src/locales/pl-PL.ts
+++ src/locales/pl-PL.ts
@@ -222,4 +222,5 @@ export default {
   TutorsPopularityMetric: 'Wskaźnik popularności nauczycieli',
   reuse_existing: 'Użyj istniejącego',
   open_new_content_editor: 'Otwórz nowy edytor treści',
+  can_skip: "Możliwość pominięcia"
 };
