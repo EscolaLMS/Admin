bd6e9ac100c9fda13b9d747a42b06d040d5ea730
Przemek <przemek.wiktorski@escola.pl>
2021-12-02 16:24:05 +0100

    changed topic types

--- src/services/escola-lms/typings.d.ts
+++ src/services/escola-lms/typings.d.ts
@@ -4,13 +4,13 @@
 declare namespace API {
   enum TopicType {
     Unselected = '',
-    RichText = 'EscolaLmsTopicTypesModelsTopicContentRichText',
-    OEmbed = 'EscolaLmsTopicTypesModelsTopicContentOEmbed',
-    Audio = 'EscolaLmsTopicTypesModelsTopicContentAudio',
-    Video = 'EscolaLmsTopicTypesModelsTopicContentVideo',
-    H5P = 'EscolaLmsTopicTypesModelsTopicContentH5P',
-    Image = 'EscolaLmsTopicTypesModelsTopicContentImage',
-    PDF = 'EscolaLmsTopicTypesModelsTopicContentPDF',
+    RichText = 'EscolaLms\\TopicTypes\\Models\\TopicContent\\RichText',
+    OEmbed = 'EscolaLms\\TopicTypes\\Models\\TopicContent\\OEmbed',
+    Audio = 'EscolaLms\\TopicTypes\\Models\\TopicContent\\Audio',
+    Video = 'EscolaLms\\TopicTypes\\Models\\TopicContent\\Video',
+    H5P = 'EscolaLms\\TopicTypes\\Models\\TopicContent\\H5P',
+    Image = 'EscolaLms\\TopicTypes\\Models\\TopicContent\\Image',
+    PDF = 'EscolaLms\\TopicTypes\\Models\\TopicContent\\PDF',
   }
 
   type Category = {
