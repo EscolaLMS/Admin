8f1fc420dd1962d8cc78a99886bd6c7870701eda
tomekgubernat <35924073+tomekgubernat@users.noreply.github.com>
2022-01-25 12:29:08 +0100

    Feature/wc 36 (#315)
    * draft
    
    * draft
    
    * draft
    
    * draft
    
    * draft
    
    * draft
    
    * assign templates for course
    
    * assign templates for course

---
+++ src/services/escola-lms/certificate.ts
@@ -0,0 +1,52 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from 'umi';
+
+/**  GET /api/admin/templates */
+export async function certificates(options?: { [key: string]: any }) {
+  return request<API.DefaultMetaResponse<API.CERTIFICATE[]>>('/api/admin/templates/assignable', {
+    method: 'GET',
+    /* useCache: true */ useCache: false,
+    ...(options || {}),
+  });
+}
+
+export async function assign(
+  id: number,
+  body?: { [key: string]: any },
+  options?: { [key: string]: any },
+) {
+  return request<API.CourseAccessList>(`/api/admin/templates/${id}/assign`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+export async function unassign(
+  id: number,
+  body?: { [key: string]: any },
+  options?: { [key: string]: any },
+) {
+  return request<API.CourseAccessList>(`/api/admin/templates/${id}/unassign`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/**  GET /api/admin/templates/assigned */
+export async function template(params: API.TemplateItem, options?: { [key: string]: any }) {
+  return request<API.DefaultMetaResponse<API.CERTIFICATE[]>>(`/api/admin/templates/assigned`, {
+    params,
+    method: 'GET',
+    /* useCache: true */ useCache: false,
+    ...(options || {}),
+  });
+}
