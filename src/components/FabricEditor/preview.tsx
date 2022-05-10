4b9555481f8680f0054de3902cdafe4816c2477f
Przemek <przemek.wiktorski@escola.pl>
2022-05-10 12:37:17 +0200

    fonts for js pdf to generate pdfs

--- src/components/FabricEditor/preview.tsx
+++ src/components/FabricEditor/preview.tsx
@@ -5,6 +5,32 @@ import 'svg2pdf.js';
 import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react';
 import type { fabric } from 'fabric';
 
+import { arialFont } from './fonts/arial';
+import { timesNewRomanFont } from './fonts/times';
+
+import { monacoFont } from './fonts/monaco';
+
+const fonts = [{ arial: 'ARIAL' }, { 'times new roman': 'times' }, { monaco: 'monaco' }];
+
+const setFonts = (doc: any) => {
+  doc.addFileToVFS('ARIAL-normal.ttf', arialFont);
+  doc.addFont('ARIAL-normal.ttf', 'ARIAL', 'normal');
+  doc.addFileToVFS('times-normal.ttf', timesNewRomanFont);
+  doc.addFont('times-normal.ttf', 'times', 'normal');
+  doc.addFileToVFS('monaco-normal.ttf', monacoFont);
+  doc.addFont('monaco-normal.ttf', 'monaco', 'normal');
+};
+
+const fontsManager = (collection: any) => {
+  collection.forEach((tspan: HTMLElement) => {
+    const currFont = tspan.style['font-family'];
+    const findFont = fonts.find((t) => currFont.toLowerCase() in t);
+    if (findFont) {
+      tspan.style.fontFamily = `${findFont[Object.keys(findFont)[0]]}`;
+    }
+  });
+};
+
 const PreviewPDF: React.FC<{
   svgDef: string;
   width: number;
@@ -14,16 +40,21 @@ const PreviewPDF: React.FC<{
   useEffect(() => {
     const doc = new jsPDF('l', 'px', [width, height]);
 
+    setFonts(doc);
+
     const parser = new DOMParser();
     const element = parser.parseFromString(svgDef, 'image/svg+xml');
 
     const bg = element.documentElement.querySelector('rect');
-
+    const texts = element.documentElement.querySelectorAll('tspan');
     // hack for background
     // svg2pdf.js don't support % units (for now)
     if (bg) {
       bg.setAttribute('width', width + 'px');
       bg.setAttribute('height', height + 'px');
+    }
+    if (texts) {
+      fontsManager(texts);
     }
 
     doc.svg(element.documentElement).then(() => {
@@ -48,23 +79,28 @@ export const FabricPreview: React.FC<{
     if (initialValue) {
       try {
         const data = typeof initialValue === 'string' ? JSON.parse(initialValue) : initialValue;
+
         canvas.loadFromJSON(data, () => {
           const svgDef = canvas.toSVG();
 
           const doc = new jsPDF('l', 'px', [width, height]);
+          setFonts(doc);
 
           const parser = new DOMParser();
           const element = parser.parseFromString(svgDef, 'image/svg+xml');
 
           const bg = element.documentElement.querySelector('rect');
+          const texts = element.documentElement.querySelectorAll('tspan');
           // const span = element.documentElement.querySelector('tspan');
           if (bg) {
             bg.setAttribute('width', width + 'px');
             bg.setAttribute('height', height + 'px');
           }
-          // if (span) {
-          //   span.setAttribute('width', width + 'px');
-          // }
+
+          if (texts) {
+            fontsManager(texts);
+          }
+
           doc.svg(element.documentElement).then(() => {
             onRendered(mode === 'file' ? doc.save('myPDF.pdf') : doc.output('blob'));
           });
