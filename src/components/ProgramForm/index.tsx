f80ef5b41d2fd3e7a6a00f3bd128d02e6053d86b
Mateusz Wojczal <mateusz@wojczal.com>
2022-07-20 16:52:03 +0200

    program form start

--- src/components/ProgramForm/index.tsx
+++ src/components/ProgramForm/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { AppContext } from '@/components/ProgramForm/Context';
 import Program from '@/components/ProgramForm/Program';
 import ThreeColProgram from '@/components/ProgramForm/ThreeColProgram';
 
-const NEW_PROGRAM = true;
+const NEW_PROGRAM = false;
 
 export const ProgramForm: React.FC<{
   id: number | string;
