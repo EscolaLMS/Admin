8e2be1ee7f5a9aeed4a2811e36958c91b37a0d7a
Mateusz Wojczal <mateusz@wojczal.com>
2022-07-26 17:29:31 +0200

    realising new type of program editingf

--- src/components/ProgramForm/index.tsx
+++ src/components/ProgramForm/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import { AppContext } from '@/components/ProgramForm/Context';
-import Program from '@/components/ProgramForm/Program';
 import ThreeColProgram from '@/components/ProgramForm/ThreeColProgram';
-
-const NEW_PROGRAM = true;
 
 export const ProgramForm: React.FC<{
   id: number | string;
 }> = ({ id }) => {
-  return <AppContext id={Number(id)}>{NEW_PROGRAM ? <ThreeColProgram /> : <Program />}</AppContext>;
+  return (
+    <AppContext id={Number(id)}>
+      <ThreeColProgram />{' '}
+    </AppContext>
+  );
 };
 
 export default ProgramForm;
