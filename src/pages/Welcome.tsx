dc61301d7a92314fda4ac4b9aa2b0e1e59801f13
Mateusz Wojczal <mateusz@wojczal.com>
2022-08-08 12:28:42 +0200

    Dashboard

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { FormattedMessage } from 'umi';
-import { Card, Typography } from 'antd';
 import { Dashdoard } from '@/components/Dashboard';
 
 export default (): React.ReactNode => {
