d2cb7fe2e2742cca84682ada1c6e8932318b737e
Piotr <piotr.kananowicz@escolasoft.com>
2022-08-11 11:34:53 +0200

    LMSDEMO-467 - Go to platform (#552)
    * LMSDEMO-468 - Global settings
    
    * static global data
    
    * autocomplete
    
    * translation
    
    * Update src/pages/Settings/components/ModalForm.tsx
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>
    
    * Update src/pages/Settings/global.tsx
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>
    
    * Update src/pages/Settings/global.tsx
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>
    
    * Update src/pages/Settings/global.tsx
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>
    
    * Update src/pages/Settings/global.tsx
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>
    
    * rename
    
    * code review
    
    * LMSDEMO-467 - Go to platform
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Dashdoard } from '@/components/Dashboard';
+import { Button } from 'antd';
+import { useModel } from '@@/plugin-model/useModel';
+import { FormattedMessage } from '@@/plugin-locale/localeExports';
 
 export default (): React.ReactNode => {
+  const { initialState } = useModel('@@initialState');
+
+  const url = initialState?.config?.filter((item) => item.key === 'frontURL')[0]?.data;
+
   return (
-    <PageContainer>
+    <PageContainer
+      extra={
+        url && (
+          <Button type="primary" href={url} target="_blank">
+            <FormattedMessage id="go_to_platform" />
+          </Button>
+        )
+      }
+    >
       <Dashdoard />
     </PageContainer>
   );
