ece5318f0d5b3292fcb1109f7bb64db4538dcd81
Mateusz Wojczal <mateusz@wojczal.com>
2022-08-24 11:10:33 +0200

    logout issue

--- src/services/token_refresh.ts
+++ src/services/token_refresh.ts
@@ -2,9 +2,10 @@ import { refreshToken } from './escola-lms/auth';
 import jwt_decode, { JwtPayload } from 'jwt-decode';
 import { differenceInSeconds } from 'date-fns';
 
-const logout = () => {
+const logout = (reason: string = 'noReason') => {
   localStorage.removeItem('TOKEN');
-  window.location.reload();
+  console.log('logout', reason);
+  //window.location.reload();
 };
 
 export const refreshTokenCallback = () => {
@@ -17,15 +18,15 @@ export const refreshTokenCallback = () => {
           localStorage.setItem('TOKEN', newToken);
           const { exp } = jwt_decode(newToken) as JwtPayload;
           if (exp) {
-            const diffInSecs = differenceInSeconds(new Date(exp * 1000), new Date());
-            setTimeout(refreshTokenCallback, (diffInSecs - 1 < 0 ? 0 : diffInSecs - 1) * 1000);
+            const diffInSecs = differenceInSeconds(new Date(exp * 1000), new Date()) - 60;
+            setTimeout(refreshTokenCallback, (diffInSecs < 0 ? 0 : diffInSecs) * 1000);
           }
         } else {
-          logout();
+          logout('r1');
         }
       })
       .catch(() => {
-        logout();
+        logout('r2');
       });
   }
 };
