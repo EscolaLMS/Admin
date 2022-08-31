7ee3c21dea9d60aba3ab39ff3c933f68a8a59046
Anna <annakozlowska24@gmail.com>
2022-08-31 08:56:20 +0200

    create new stationary event test

---
+++ src/e2e/newStationaryEvents.e2e.spec.ts
@@ -0,0 +1,38 @@
+import { expect, test } from '@playwright/test';
+import { BASE_URL, ADMIN_CREDENTIALS } from './consts';
+
+test.describe('New stationary events', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`${BASE_URL}/#/user/login`);
+    await page.waitForSelector('#email');
+
+    await page.locator('input[id="email"]').fill(ADMIN_CREDENTIALS.email);
+    await page.locator('input[id="password"]').fill(ADMIN_CREDENTIALS.password);
+    await page.locator('form button').click();
+
+    await expect(page).toHaveURL(/.*welcome/);
+  });
+
+  test('create new stationary events', async ({ page }) => {
+    await page.goto(`${BASE_URL}/#/other/stationary-events`);
+    await page.locator('text=new').click();
+    await expect(page).toHaveURL(`${BASE_URL}/#/other/stationary-events/new`);
+    await page.locator('#name').fill('new event');
+    await page.locator('#place').fill('Warsaw');
+    await page.locator('.ant-select-selection-item').click();
+    await page.locator('text=Draft').nth(1).click();
+    await page.locator('#started_at').click();
+    await page.locator('text=Today >> nth=0').click();
+    await page.locator('#finished_at').click();
+    await page.locator('text=Today >> nth=1').click();
+    await page.locator("div[role='textbox'] >> nth=1").fill('test description');
+    await page.locator('button:has-text("Submit")').click();
+
+    page.once('dialog', (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`);
+      dialog.dismiss().catch(() => {});
+    });
+
+    await page.waitForSelector('text=Stationary event saved successfully', { state: 'visible' });
+  });
+});
