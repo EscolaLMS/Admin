aea882a271eb8f5a75ef900ac5237922b4bd2f59
Mateusz Wojczal <mateusz@wojczal.com>
2022-09-01 15:50:33 +0200

    login

--- src/e2e/helpers.ts
+++ src/e2e/helpers.ts
@@ -1,5 +1,5 @@
 import type { Page } from '@playwright/test';
-
+import { expect } from '@playwright/test';
 import { BASE_URL, ADMIN_CREDENTIALS } from './consts';
 
 export const loginAsAdmin = async (page: Page) => {
