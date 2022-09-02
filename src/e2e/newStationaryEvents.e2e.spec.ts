44c0c31c02979f7f061b99cde51a95548ed86d0c
Anna <annakozlowska24@gmail.com>
2022-09-02 08:33:29 +0200

    add delete stationary event

--- src/e2e/newStationaryEvents.e2e.spec.ts
+++ src/e2e/newStationaryEvents.e2e.spec.ts
@@ -2,12 +2,12 @@ import { expect, test } from '@playwright/test';
 import { BASE_URL } from './consts';
 import { loginAsAdmin } from './helpers';
 
-test.describe('New stationary events', () => {
+test.describe.only('New stationary events', () => {
   test.beforeEach(async ({ page }) => {
     await loginAsAdmin(page);
   });
 
-  test('create new stationary events', async ({ page }) => {
+  test('create and delete stationary events', async ({ page }) => {
     await page.goto(`${BASE_URL}/#/other/stationary-events`);
     await page.locator('text=new').click();
     await expect(page).toHaveURL(`${BASE_URL}/#/other/stationary-events/new`);
@@ -28,5 +28,15 @@ test.describe('New stationary events', () => {
     });
 
     await page.waitForSelector('text=Stationary event saved successfully', { state: 'visible' });
+
+    await page.goto(`${BASE_URL}/#/other/stationary-events`);
+    await page.waitForTimeout(3500);
+    await page.locator('#name').fill('new event');
+    await page.locator('button:has-text("Query")').click();
+    await page.locator('text=new event2022 >> button').nth(1).click();
+    const ConfirmDeleteCourse = await page.locator('.ant-popover-message');
+    await expect(ConfirmDeleteCourse).toContainText('Are you sure to delete this record?');
+    await page.locator('button:has-text("Yes")').click();
+    await page.waitForSelector('text=Stationary event deleted successfully', { state: 'visible' });
   });
 });
