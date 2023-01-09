d71f600c41aa9caec65179b1b49ac610f0ee7b26
Mateusz Wojczal <mateusz@wojczal.com>
2023-01-09 16:26:42 +0100

    sentry & ybug image buyild

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,6 @@ sed -ie "s~API_URL = null~API_URL=\"$API_URL\"~" /usr/local/apache2/htdocs/index
 sed -ie "s~SENTRYDSN = null~SENTRYDSN=\"$SENTRYDSN\"~" /usr/local/apache2/htdocs/index.html
 sed -ie "s~YBUG = null~YBUG=\"$YBUG\"~" /usr/local/apache2/htdocs/index.html
 
-sed -ie "s~API_URL = null~API_URL=\"$API_URL\"~"  dist/index.html
-sed -ie "s~SENTRYDSN = null~SENTRYDSN=\"$SENTRYDSN\"~"  dist/index.html
-sed -ie "s~YBUG = null~YBUG=\"$YBUG\"~"  dist/index.html
-
 echo "API URL= " $API_URL
 echo "SENTRYDSN= " $SENTRYDSN
 echo "YBUG= " $YBUG
