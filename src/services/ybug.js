cc71ce1919480f2fa8543b3ff87132b1ccccbdd4
Mateusz Qunabu <mateusz@qunabu.com>
2023-01-09 13:12:53 +0100

    Update ybug.js

--- src/services/ybug.js
+++ src/services/ybug.js
@@ -1,11 +1,13 @@
-try {
-  window.ybug_settings = { id: 'b307ykjnw50dx3pg1r2y' };
-  const ybug = document.createElement('script');
-  ybug.type = 'text/javascript';
-  ybug.async = true;
-  ybug.src = `https://widget.ybug.io/button/${window.ybug_settings.id}.js`;
-  const s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ybug, s);
-} catch (er) {
-  console.error('ybug error', er);
+if (process.env.REACT_APP_YBUG) {
+  try {
+    window.ybug_settings = { id: process.env.REACT_APP_YBUG };
+    const ybug = document.createElement('script');
+    ybug.type = 'text/javascript';
+    ybug.async = true;
+    ybug.src = `https://widget.ybug.io/button/${window.ybug_settings.id}.js`;
+    const s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ybug, s);
+  } catch (er) {
+    console.error('ybug error', er);
+  }
 }
