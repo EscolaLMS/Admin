b928c2af2a9f43bc53b03b21f36287d8eb5d818e
Szymon Dolnik <42141039+Haxikowy@users.noreply.github.com>
2023-04-19 17:10:01 +0200

    feature: EditUser groups (#790)
    * Fix for project topic type not showing in add topic
    
    * added type to service
    
    * user group
    
    * ParentLesson root naming fix

--- src/pages/Users/User/form.tsx
+++ src/pages/Users/User/form.tsx
@@ -1,16 +1,87 @@
 import React, { useMemo, useState, useEffect } from 'react';
-import { message, Spin, Form, Button, Space, Typography, Divider } from 'antd';
+import { message, Spin, Form, Button, Space, Typography, Divider, Row } from 'antd';
 import ProForm, { ProFormText, ProFormSwitch, ProFormCheckbox } from '@ant-design/pro-form';
 import { updateUser, createUser, resendEmail } from '@/services/escola-lms/user';
 import SecureUploadBrowser from '@/components/SecureUpload/browser';
 import ResponsiveImage from '@/components/ResponsiveImage';
+import UserGroupSelect from '@/components/UserGroupSelect';
 import { useParams, history, useModel, Link } from 'umi';
 import { useCallback } from 'react';
 import { useIntl, FormattedMessage } from 'umi';
 import { roles as getRoles } from '@/services/escola-lms/roles';
 import { deleteUserAvatar } from '@/services/escola-lms/user';
+import {
+  userGroupsTree,
+  addUserToGroup,
+  removeUserFromGroup,
+} from '@/services/escola-lms/user_groups';
 import useModelFields from '@/hooks/useModelFields';
 import AdditionalField from './components/AdditionalField';
+
+function useUserGroups(user_id: number) {
+  const [userGroups, setUserGroups] = useState<number[]>([]);
+  const [userGroupsLoading, setUserGroupsLoading] = useState(false);
+
+  const fetchUserGroups = useCallback(async () => {
+    try {
+      setUserGroupsLoading(true);
+      const response = await userGroupsTree({ user_id });
+
+      if (response.success) {
+        setUserGroups(response.data.map(({ id }) => id));
+      }
+    } finally {
+      setUserGroupsLoading(false);
+    }
+  }, [user_id]);
+
+  const handleAddToGroup = useCallback(
+    async (group_id: string | number) => {
+      if (typeof group_id === 'string') return;
+      setUserGroupsLoading(true);
+
+      await addUserToGroup(group_id, user_id);
+      await fetchUserGroups();
+
+      setUserGroupsLoading(false);
+    },
+    [user_id],
+  );
+
+  const handleRemoveFromGroup = useCallback(
+    async (group_id: string | number) => {
+      if (typeof group_id === 'string') return;
+      setUserGroupsLoading(true);
+
+      await removeUserFromGroup(group_id, user_id);
+      await fetchUserGroups();
+
+      setUserGroupsLoading(false);
+    },
+    [user_id],
+  );
+
+  const handleClearAllGroups = useCallback(async () => {
+    setUserGroupsLoading(true);
+
+    await Promise.all(userGroups.map((group_id) => removeUserFromGroup(group_id, user_id)));
+    await fetchUserGroups();
+
+    setUserGroupsLoading(false);
+  }, [userGroups]);
+
+  useEffect(() => {
+    fetchUserGroups();
+  }, [fetchUserGroups]);
+
+  return {
+    userGroups,
+    userGroupsLoading,
+    handleAddToGroup,
+    handleRemoveFromGroup,
+    handleClearAllGroups,
+  };
+}
 
 export default ({
   isNew,
@@ -27,6 +98,14 @@ export default ({
   const additionalFields = useModelFields('EscolaLms\\Auth\\Models\\User');
   const { initialState } = useModel('@@initialState');
   const baseUrl = initialState?.config?.filter((item) => item.key === 'frontURL')[0]?.data;
+
+  const {
+    userGroups,
+    userGroupsLoading,
+    handleAddToGroup,
+    handleRemoveFromGroup,
+    handleClearAllGroups,
+  } = useUserGroups(data?.id ?? 0);
 
   const [roles, setRoles] = useState<API.Role[]>();
 
@@ -210,42 +289,60 @@ export default ({
           />
         )}
       </ProForm.Group>
-      <Divider />
       {!isNew && (
-        <ProForm.Group>
-          <ProForm.Item name="avatar" label={<FormattedMessage id="avatar" />}>
-            {data.path_avatar ? (
-              <ResponsiveImage path={data.path_avatar} size={600} width={200} />
-            ) : (
-              <Typography>
-                <FormattedMessage id="avatar_placeholder" />
-              </Typography>
-            )}
-          </ProForm.Item>
-          <Form.Item noStyle shouldUpdate>
-            {() => (
-              <>
-                <SecureUploadBrowser
-                  folder={`avatars/${user}`}
-                  wrapInForm={false}
-                  url={`/api/admin/users/${user}/avatar`}
-                  name="avatar"
-                  accept="image/*"
-                  onChange={(info) => {
-                    if (info.file.status === 'done') {
-                      if (info.file.response && info.file.response.success) {
-                        setData(info.file.response.data);
+        <>
+          <ProForm.Group>
+            <ProForm.Item name="avatar" label={<FormattedMessage id="avatar" />}>
+              {data.path_avatar ? (
+                <ResponsiveImage path={data.path_avatar} size={600} width={200} />
+              ) : (
+                <Typography>
+                  <FormattedMessage id="avatar_placeholder" />
+                </Typography>
+              )}
+            </ProForm.Item>
+            <Form.Item noStyle shouldUpdate>
+              {() => (
+                <>
+                  <SecureUploadBrowser
+                    folder={`avatars/${user}`}
+                    wrapInForm={false}
+                    url={`/api/admin/users/${user}/avatar`}
+                    name="avatar"
+                    accept="image/*"
+                    onChange={(info) => {
+                      if (info.file.status === 'done') {
+                        if (info.file.response && info.file.response.success) {
+                          setData(info.file.response.data);
+                        }
                       }
-                    }
-                  }}
-                />
-                <Button danger onClick={onDeleteAvatar}>
-                  <FormattedMessage id="delete" />
-                </Button>
-              </>
-            )}
-          </Form.Item>
-        </ProForm.Group>
+                    }}
+                  />
+                  <Button danger onClick={onDeleteAvatar}>
+                    <FormattedMessage id="delete" />
+                  </Button>
+                </>
+              )}
+            </Form.Item>
+          </ProForm.Group>
+          <Row>
+            <Divider />
+            <ProForm.Item
+              style={{ width: '100%' }}
+              label={<FormattedMessage id="groups" defaultMessage="Groups" />}
+            >
+              <UserGroupSelect
+                multiple
+                loading={userGroupsLoading}
+                disabled={userGroupsLoading}
+                value={userGroups}
+                onSelect={handleAddToGroup}
+                onDeselect={handleRemoveFromGroup}
+                onClear={handleClearAllGroups}
+              />
+            </ProForm.Item>
+          </Row>
+        </>
       )}
     </ProForm>
   );
