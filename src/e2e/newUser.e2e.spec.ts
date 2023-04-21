e8ad31111d39014ddc5c4e1b36a91e6f8b078ee0
Jacek <jac.wojowski@gmail.com>
2023-04-21 14:24:06 +0200

    Automated tests - NewUser fix

--- src/e2e/newUser.e2e.spec.ts
+++ src/e2e/newUser.e2e.spec.ts
@@ -7,8 +7,18 @@ test.describe('New user', () => {
     await loginAsAdmin(page);
   });
 
-  test('create new user', async ({ page }) => {
+  test.only('create new user', async ({ page }) => {
     const email = 'newuser' + getDateTicks() + '@pwtest.pl';
+
+    
+    await page.goto(`${BASE_URL}/#/configuration/settings/escola_auth`);
+    await page.waitForLoadState();
+    await page.waitForSelector('text=Settings', { state: 'visible' });
+    await page.click('.anticon-edit >>nth=2')
+    await page.locator('.ant-input').fill('http://localhost');
+    await page.click('text=OK');
+    await page.waitForTimeout(2000)
+    await page.waitForLoadState();
 
     await page.goto(`${BASE_URL}/#/users/list`);
     await page.locator('text=new').click();
@@ -17,28 +27,30 @@ test.describe('New user', () => {
     await page.type('#last_name', 'user');
     await page.type('#email', email);
     await page.type('#password', 'Testowanie1!');
-    await page.waitForTimeout(10000);
-    await page.click('.ant-checkbox-input >> nth = 0');
-    await page.click('.ant-checkbox-input >> nth = 1');
+    await page.waitForTimeout(10000)
+    await page.click('.ant-checkbox-input >> nth = 0')
+    await page.click('.ant-checkbox-input >> nth = 1')
     await page.locator('#is_active').click();
     await page.locator('text=student').click();
     await page.locator('button:has-text("Submit")').click();
     // await page.waitForSelector('text=Created user', { state: 'visible' });
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(7000)
 
     await page.goto(`${BASE_URL}/#/users/list`);
-    await page.waitForTimeout(6000);
+    await page.waitForTimeout(4000);
     await page.locator('#search').fill(email);
-    await page.waitForTimeout(5000);
-    await page.getByRole('button', { name: /Query/i }).click();
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(3000)
+    await page.getByRole('button', {name: /Query/i }).click()
     // await page.locator('text=@pwtest.plActiveUnverifiedstudent- >> button').nth(1).click();
-    await page.click('.anticon-delete>>nth=0');
+    await page.click('.anticon-delete>>nth=0')
 
+
+  
     const ConfirmDeleteUser = await page.locator('.ant-popover-message');
     await expect(ConfirmDeleteUser).toContainText('Are you sure to delete this record?');
     await page.locator('button:has-text("Yes")').click();
     // await page.waitForSelector('text=No Data', { state: 'visible' });
+    
   });
 });
 
