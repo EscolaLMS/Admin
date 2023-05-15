f921a605d70d0b7a14e4e3d5fc3ef7ffe23c52ae
Szymon Dolnik <szymon2407@hotmail.com>
2023-05-15 15:38:18 +0200

    Added groupUsers to TeacherSubjectContext

--- src/pages/TeacherSubjects/context.tsx
+++ src/pages/TeacherSubjects/context.tsx
@@ -1,14 +1,24 @@
+import { studentUserGroup } from '@/services/escola-lms/student_user_groups';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { semesterSubject } from '@/services/escola-lms/semesterSubject';
 
+interface GroupUserData {
+  loading: boolean;
+  byId: Record<string, API.StudentUserGroup>;
+}
+
 export interface TeacherSubjectContext {
   teacherSubjectData: Partial<API.Subjects> | null;
+  groupUsers: GroupUserData;
   getTeacherSubjectById: (semester_subject_id: number) => void;
+  getGroupUsers: (group_id: number) => void;
 }
 
 const Context = React.createContext<TeacherSubjectContext>({
   teacherSubjectData: null,
-  getTeacherSubjectById: () => console.warn('Default value!'),
+  groupUsers: { loading: false, byId: {} },
+  getTeacherSubjectById: () => console.warn('default value'),
+  getGroupUsers: () => console.warn('default value'),
 });
 
 export const useTeacherSubject = () => useContext(Context);
@@ -18,6 +28,7 @@ export const TeacherSubjectContextProvider: React.FC<{
   semester_subject_id: number;
 }> = ({ children, semester_subject_id }) => {
   const [teacherSubjectData, setTeacherSubjectData] = useState<Partial<API.Subjects> | null>(null);
+  const [groupUsers, setGroupUsers] = useState<GroupUserData>({ loading: false, byId: {} });
 
   const getTeacherSubjectById = useCallback(async (semesterSubjectId: number) => {
     const response = await semesterSubject(semesterSubjectId);
@@ -29,12 +40,26 @@ export const TeacherSubjectContextProvider: React.FC<{
     }
   }, []);
 
+  const getGroupUsers = useCallback(async (group_id: number) => {
+    setGroupUsers((prev) => ({ ...prev, loading: true }));
+    const response = await studentUserGroup(group_id);
+
+    if (response.success) {
+      setGroupUsers((prev) => ({
+        loading: false,
+        byId: { ...prev.byId, [group_id]: response.data },
+      }));
+    }
+  }, []);
+
   useEffect(() => {
     getTeacherSubjectById(semester_subject_id);
   }, [getTeacherSubjectById, semester_subject_id]);
 
   return (
-    <Context.Provider value={{ teacherSubjectData, getTeacherSubjectById }}>
+    <Context.Provider
+      value={{ teacherSubjectData, getTeacherSubjectById, groupUsers, getGroupUsers }}
+    >
       {children}
     </Context.Provider>
   );
