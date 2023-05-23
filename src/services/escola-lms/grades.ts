2ce484f6d879022f8bc4960c4b375f8d0cbdaea3
Szymon Dolnik <42141039+Haxikowy@users.noreply.github.com>
2023-05-23 12:48:58 +0200

    feat: Final grades (#835)
    * getGroupFinalGrades method & types
    
    * FinalGradesList
    
    * FinalGradesDetails setup
    
    * FinalGradesDetails heading, form, grade scales table, attendance table. Added a few grades methods
    
    * create & update final grade api methods
    
    * Form submission
    
    * FinalGradesList refactor
    
    * Params update
    
    * Exams table
    
    * Final grades table
    
    * ProposedGrade
    
    * max percent -> min percent
    
    * GradesScale show permission refactor
    
    * course stats params refactor
    
    * userProgress.tsx types refactor
    
    * grade scales refactor
    
    * User course progress
    
    * Deleted non-nullish type assertion
    
    * Missing translations
    
    * group id in final grades list
    
    * Added colors to tags in user progress
    
    * Translation fix
    
    * Proposed grades fix
    
    * FinalGradesList group multi select
    
    * allowClear

--- src/services/escola-lms/grades.ts
+++ src/services/escola-lms/grades.ts
@@ -33,3 +33,84 @@ export async function createSubjectTutorGrades(
     },
   );
 }
+
+/**  GET /api/admin/lesson-group-users */
+export async function getGroupFinalGrades(
+  group_id: number | number[],
+  options?: RequestOptionsInit,
+) {
+  return request<API.DefaultResponse<API.FinalGradeItem[]>>(`/api/admin/lesson-group-users`, {
+    method: 'GET',
+    params: { 'groups[]': group_id },
+    /* useCache: true */ useCache: false,
+    ...(options || {}),
+  });
+}
+
+/**  GET /api/admin/lesson-group-users/groups/:group_id/users/:user_id */
+export async function getUserFinalGrades(
+  group_id: number,
+  user_id: number,
+  options?: RequestOptionsInit,
+) {
+  return request<API.DefaultResponse<API.FinalGradeItem>>(
+    `/api/admin/lesson-group-users/groups/${group_id}/users/${user_id}`,
+    {
+      method: 'GET',
+      /* useCache: true */ useCache: false,
+      ...(options || {}),
+    },
+  );
+}
+
+/** GET /api/admin/grade-terms */
+export async function getGradeTerms(options?: RequestOptionsInit) {
+  return request<API.DefaultResponse<API.GradeTerm[]>>(`/api/admin/grade-terms`, {
+    method: 'GET',
+    /* useCache: true */ useCache: false,
+    ...(options || {}),
+  });
+}
+
+/** GET /api/admin/grade-scales/:s_subject_scale_form_id */
+export async function getSubjectGradeScales(
+  s_subject_scale_form_id: number,
+  options?: RequestOptionsInit,
+) {
+  return request<API.DefaultResponse<API.SubjectGradeScale[]>>(
+    `/api/admin/grade-scales/${s_subject_scale_form_id}`,
+    {
+      method: 'GET',
+      /* useCache: true */ useCache: false,
+      ...(options || {}),
+    },
+  );
+}
+
+/** POST /api/admin/final-grades */
+export async function createFinalGrade(
+  body: API.CreateFinalGradeRequest,
+  options?: RequestOptionsInit,
+) {
+  return request<API.DefaultResponse<API.FinalGradeItem>>(`/api/admin/final-grades`, {
+    method: 'POST',
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** PATCH /api/admin/final-grades/:id */
+export async function updateFinalGrade(
+  final_grade_id: number,
+  body: API.UpdateFinalGradeRequest,
+  options?: RequestOptionsInit,
+) {
+  return request<API.DefaultResponse<API.FinalGradeItem>>(
+    `/api/admin/final-grades/${final_grade_id}`,
+    {
+      method: 'PATCH',
+      data: body,
+      ...(options || {}),
+    },
+  );
+}
