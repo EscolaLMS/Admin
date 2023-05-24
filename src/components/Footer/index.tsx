c5aaa731b706fac60bb06fad07587371b511d59b
myslaf <przemodesign@gmail.com>
2023-05-24 17:07:10 +0200

    Get company name and url from settings.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,41 @@
 // import React from 'react';
 import { GithubOutlined } from '@ant-design/icons';
 import DefaultFooter from '@ant-design/pro-layout/lib/Footer';
+import { useModel } from '@@/plugin-model/useModel';
+import { useMemo } from 'react';
 
-export default () => (
-  <DefaultFooter
-    copyright="2021 EscolaSoft"
-    links={[
-      {
-        key: 'Wellms',
-        title: 'Wellms',
-        href: 'https://www.wellms.io/',
-        blankTarget: true,
-      },
-      {
-        key: 'github',
-        title: <GithubOutlined />,
-        href: 'https://github.com/EscolaLMS',
-        blankTarget: true,
-      },
-    ]}
-  />
-);
+export default () => {
+  const { initialState } = useModel('@@initialState');
+  const companyName = useMemo(
+    () =>
+      initialState?.config?.find((item) => item.group === 'companyInfo' && item.key === 'name')
+        ?.value,
+    [initialState?.config],
+  );
+  const companyUrl = useMemo(
+    () =>
+      initialState?.config?.find((item) => item.group === 'companyInfo' && item.key === 'url')
+        ?.value,
+    [initialState?.config],
+  );
+
+  return (
+    <DefaultFooter
+      copyright="2021 EscolaSoft"
+      links={[
+        {
+          key: companyName || 'Wellms',
+          title: companyName || 'Wellms',
+          href: companyUrl || 'https://www.wellms.io/',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/EscolaLMS',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
