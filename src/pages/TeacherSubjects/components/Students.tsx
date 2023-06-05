5ce8bc3a2df802caebe77a36f688e2e2528a5789
Szymon Dolnik <42141039+Haxikowy@users.noreply.github.com>
2023-06-05 14:03:16 +0200

    feat: Students table search refactor SUD-172 (#887)
    * Students base refactor & student first_name, last_name & email filters
    
    * Groups select
    
    * rowKey fix

--- src/pages/TeacherSubjects/components/Students.tsx
+++ src/pages/TeacherSubjects/components/Students.tsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useMemo } from 'react';
 import { FormattedMessage, Link } from 'umi';
-import { Button, Select, Space, Tooltip, Typography } from 'antd';
-import type { ActionType, ProColumns } from '@ant-design/pro-table';
-import ProTable from '@ant-design/pro-table';
+import { Button, Tooltip } from 'antd';
+import type { DefaultOptionType } from 'antd/lib/select';
+import ProTable, { type ProColumns } from '@ant-design/pro-table';
 import { ExportOutlined } from '@ant-design/icons';
-import { useTeacherSubject } from '../context';
+
 import { allStudentsAndGroups as fetchAllStudentsAndGroups } from '@/services/escola-lms/student_user_groups';
-import ProCard from '@ant-design/pro-card';
+import { useTeacherSubject } from '../context';
 
 interface TableDataProps {
   group_id: number;
   group_name: string;
   user_id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+}
+
+interface TableParams {
+  groups: number[];
   first_name: string;
   last_name: string;
   email: string;
@@ -26,17 +33,14 @@ export const TableColumns: ProColumns<TableDataProps>[] = [
   {
     title: <FormattedMessage id="first_name" defaultMessage="First name" />,
     dataIndex: 'first_name',
-    hideInSearch: true,
   },
   {
     title: <FormattedMessage id="last_name" defaultMessage="Last name" />,
     dataIndex: 'last_name',
-    hideInSearch: true,
   },
   {
     title: <FormattedMessage id="email" defaultMessage="email" />,
     dataIndex: 'email',
-    hideInSearch: true,
   },
   {
     title: <FormattedMessage id="groupName" defaultMessage="Group name" />,
@@ -45,102 +49,84 @@ export const TableColumns: ProColumns<TableDataProps>[] = [
   },
 ];
 
-export const Students: React.FC = () => {
-  const [loading, setLoading] = useState(false);
-  const { teacherSubjectData } = useTeacherSubject();
-  const [studentsAndGroups, setStudentsAndGroup] = useState<API.StudentUserGroup[]>([]);
-  const actionRef = useRef<ActionType>();
+const getGroupsOptions = (subjectGroups: API.SubjectGroups[]): DefaultOptionType[] =>
+  subjectGroups.map(({ id, name }) => ({
+    label: name,
+    value: id,
+  }));
 
-  const tableData: TableDataProps[] = useMemo(
-    () =>
-      studentsAndGroups.reduce<TableDataProps[]>((acc, curr) => {
-        const userTable: TableDataProps[] = [];
-        curr?.users?.reduce((accumulator, currentUser) => {
-          const user = {
-            group_id: curr.id,
-            group_name: curr.name,
-            user_id: currentUser.id,
-            first_name: currentUser.first_name,
-            last_name: currentUser.last_name,
-            email: currentUser.email,
-          };
-          userTable.push(user);
-          return { ...accumulator, ...user };
-        }, {});
+const getTableData = (studentUserGroups: API.StudentUserGroup[]): TableDataProps[] =>
+  studentUserGroups.reduce<TableDataProps[]>((acc, curr) => {
+    const userTable = curr?.users?.map<TableDataProps>((currentUser) => ({
+      group_id: curr.id,
+      group_name: curr.name,
+      user_id: currentUser.id,
+      first_name: currentUser.first_name,
+      last_name: currentUser.last_name,
+      email: currentUser.email,
+    }));
 
-        setLoading(false);
-        return [...acc, ...userTable];
-      }, []),
-    [studentsAndGroups],
-  );
-
-  const fetchStudentsData = (value?: number[]) => {
-    fetchAllStudentsAndGroups({ parent_id: teacherSubjectData?.group_id, 'id[]': value }).then(
-      (response) => {
-        if (response.success) {
-          setStudentsAndGroup(response.data);
-        }
-      },
-    );
-  };
-
-  const handleSelectGroup = (value: number[]) => {
-    setLoading(true);
-    fetchStudentsData(value);
-  };
-
-  useEffect(() => {
-    fetchStudentsData();
+    return [...acc, ...userTable];
   }, []);
 
-  return (
-    <>
-      <ProCard>
-        <Space>
-          <Typography.Title level={5}>
-            <FormattedMessage id="select_user_group" defaultMessage="Select group" />
-          </Typography.Title>
-          <Select
-            allowClear
-            mode="multiple"
-            style={{ minWidth: '400px' }}
-            onChange={handleSelectGroup}
-            onClear={fetchStudentsData}
-          >
-            {teacherSubjectData?.groups?.map((group: API.SubjectGroups) => (
-              <Select.Option key={group.id} value={group.id}>
-                {group.name}
-              </Select.Option>
-            ))}
-          </Select>
-        </Space>
-      </ProCard>
+export const Students: React.FC = () => {
+  const { teacherSubjectData } = useTeacherSubject();
 
-      <ProTable
-        actionRef={actionRef}
-        rowKey="user_id"
-        search={false}
-        loading={loading}
-        dataSource={tableData ?? []}
-        columns={[
-          ...TableColumns,
-          {
-            hideInSearch: true,
-            title: <FormattedMessage id="msTeams" />,
-            dataIndex: 'teamsLink',
-            valueType: 'option',
-            render: () => [
-              <Link to={'#'} key="teamsLink">
-                <Tooltip title={<FormattedMessage id="msTeams" defaultMessage="teams" />}>
-                  <Button type="primary" icon={<ExportOutlined />} />
-                </Tooltip>
-              </Link>,
-            ],
+  const groupOptions = useMemo(
+    () => getGroupsOptions(teacherSubjectData?.groups ?? []),
+    [teacherSubjectData?.groups],
+  );
+
+  return (
+    <ProTable<TableDataProps, TableParams>
+      rowKey={(record) => `${record.group_id}-${record.user_id}`}
+      search={{ layout: 'vertical' }}
+      request={async ({ groups, first_name = '', last_name = '', email = '' }) => {
+        const response = await fetchAllStudentsAndGroups({
+          parent_id: teacherSubjectData?.group_id,
+          'id[]': groups,
+        });
+
+        if (!response.success) {
+          return { data: [], total: 0, success: false };
+        }
+
+        const tableData = getTableData(response.data).filter(
+          (record) =>
+            record.first_name.toLowerCase().includes(first_name.toLowerCase()) &&
+            record.last_name.toLowerCase().includes(last_name.toLowerCase()) &&
+            record.email.toLowerCase().includes(email.toLowerCase()),
+        );
+
+        return { data: tableData, total: tableData.length, success: true };
+      }}
+      columns={[
+        {
+          title: <FormattedMessage id="groups" defaultMessage="Groups" />,
+          dataIndex: 'groups',
+          hideInTable: true,
+          filterMultiple: true,
+          valueType: 'select',
+          fieldProps: {
+            options: groupOptions,
+            mode: 'multiple',
           },
-        ]}
-      />
-    </>
+        },
+        ...TableColumns,
+        {
+          hideInSearch: true,
+          title: <FormattedMessage id="msTeams" />,
+          dataIndex: 'teamsLink',
+          valueType: 'option',
+          render: () => [
+            <Link to={'#'} key="teamsLink">
+              <Tooltip title={<FormattedMessage id="msTeams" defaultMessage="teams" />}>
+                <Button type="primary" icon={<ExportOutlined />} />
+              </Tooltip>
+            </Link>,
+          ],
+        },
+      ]}
+    />
   );
 };
-
-export default Students;
