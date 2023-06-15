317a00c09dd47c2d8948fa7a825f282dd35d46d6
Przemek <przemodesign@gmail.com>
2023-06-15 11:31:09 +0200

    Add translation for duration tooltip in consultations. (#896)

--- src/locales/fr-FR/consultations.ts
+++ src/locales/fr-FR/consultations.ts
@@ -5,4 +5,6 @@ export default {
   'consultations.proposed_terms': 'Dates proposées',
   'consultations.calendar': 'Calendrier',
   'consultations.edit': 'Modifier la consultation',
+  'consultations.duration_tooltip':
+    'Durée de la consultation en heures, minutes, secondes, jours, semaines, mois ou un an. Exemples : 2 heures, 20 minutes, 1 jour ou 1 heure et 30 minutes combinées.',
 };
