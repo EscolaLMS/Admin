71c764842dea5c2e6cc376b04e7dc6e975ea3207
Szymon Dolnik <szymon2407@hotmail.com>
2023-06-16 11:13:52 +0200

    Delete redundant api methods - we use only umi-request to make requests, and we have REACT_APP_API_URL env

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,3 @@
-export const APIpath = (action) => `http://localhost:1000/api/${action}`;
-
-export const API = (action, token, method = 'GET', data = null) => {
-  const opts = {
-    method,
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    // mode: "no-cors",
-    // credentials: 'include',
-  };
-  if (data) {
-    opts.body = data;
-  } else {
-    opts.headers['Content-Type'] = 'application/json';
-  }
-  return fetch(APIpath(action), opts);
-};
-
 export const getFormData = (data) => {
   const formData = new FormData();
 
@@ -38,5 +18,3 @@ export const getFormData = (data) => {
   }
   return formData;
 };
-
-export default API;
