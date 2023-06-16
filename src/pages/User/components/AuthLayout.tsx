1d169c933cde827a1b244e57a37e3ef509aae18f
Szymon Dolnik <szymon2407@hotmail.com>
2023-06-16 16:34:49 +0200

    Custom logo on login page

--- src/pages/User/components/AuthLayout.tsx
+++ src/pages/User/components/AuthLayout.tsx
@@ -1,10 +1,27 @@
-import type { PropsWithChildren } from 'react';
+import React, { type PropsWithChildren, useMemo } from 'react';
 import Footer from '@/components/Footer';
 import styles from './index.less';
-import { Link, SelectLang } from 'umi';
+import { Link, SelectLang, useModel } from 'umi';
 import { Divider } from 'antd';
 
+declare const REACT_APP_API_URL: string;
+
 const AuthLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  const { initialState } = useModel('@@initialState');
+
+  const configLogo = useMemo(() => {
+    const logo = initialState?.publicConfig?.global?.logoLogin;
+
+    if (!logo) {
+      return '';
+    }
+    if (!logo.includes('http')) {
+      return `${window.REACT_APP_API_URL || REACT_APP_API_URL}/storage${logo}`;
+    }
+
+    return logo;
+  }, [initialState?.publicConfig]);
+
   return (
     <div className={styles.container}>
       <div className={styles.lang}>{SelectLang && <SelectLang />}</div>
@@ -12,7 +29,11 @@ const AuthLayout: React.FC<PropsWithChildren> = ({ children }) => {
         <div className={styles.top}>
           <div className={styles.header}>
             <Link to="/">
-              <img alt="logo" className={styles.logo} src="/logo.svg" />
+              <img
+                alt="logo"
+                className={configLogo ? styles['custom-logo'] : styles.logo}
+                src={configLogo || '/logo.svg'}
+              />
             </Link>
           </div>
         </div>
