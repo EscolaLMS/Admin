376e6c86ff4c6a82b0c7a1aa28bbeb440b227820
Szymon Dolnik <szymon2407@hotmail.com>
2023-07-10 12:14:21 +0200

    Table refactor

--- src/components/GiftQuizQuestions/table.tsx
+++ src/components/GiftQuizQuestions/table.tsx
@@ -1,27 +1,83 @@
-import { MenuOutlined } from '@ant-design/icons';
-import { ProTable } from '@ant-design/pro-table';
-import { Fragment, useState, useCallback, useRef, useEffect } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
+import { FormattedMessage } from 'umi';
 import { SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
 import { arrayMoveImmutable } from 'array-move';
-import './index.css';
 import { parse } from '@escolalms/gift-pegjs';
-import { Button, Space, Drawer, Typography, Switch } from 'antd';
-import { FormattedMessage } from 'umi';
+import { Button, Drawer, Typography, Switch } from 'antd';
+import { MenuOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { ProTable } from '@ant-design/pro-table';
+import type { ProColumns, ActionType } from '@ant-design/pro-table';
+
 import { createQuestion, deleteQuestion, updateQuestion } from '@/services/escola-lms/gift_quiz';
-import { EditQuestion } from './editor/edit_question';
 import { GiftQuizNewQuestionEditor } from './editor/new_question';
-import { PlusCircleOutlined } from '@ant-design/icons';
-import type { ActionType } from '@ant-design/pro-table';
+import { EditQuestion } from './editor/edit_question';
+import './index.css';
 
 const DragHandle = SortableHandle(() => <MenuOutlined style={{ cursor: 'grab', color: '#999' }} />);
+const SortableItem = SortableElement((props: React.HTMLAttributes<HTMLTableRowElement>) => (
+  <tr {...props} />
+));
+const SortContainer = SortableContainer((props: React.HTMLAttributes<HTMLTableSectionElement>) => (
+  <tbody {...props} />
+));
 
-export const Table: React.FC<{
+const staticColumns: ProColumns<API.GiftQuestion>[] = [
+  {
+    title: <FormattedMessage id="sort" defaultMessage="sort" />,
+    dataIndex: 'id',
+    width: 60,
+    className: 'drag-visible',
+    render: () => <DragHandle />,
+  },
+  {
+    title: <FormattedMessage id="type" defaultMessage="Type" />,
+    dataIndex: 'type',
+    hideInForm: true,
+    hideInSearch: true,
+    hideInSetting: true,
+    render: (_, row: API.GiftQuestion) => <FormattedMessage id={row.type} />,
+  },
+  {
+    title: <FormattedMessage id="question" defaultMessage="Question" />,
+    dataIndex: 'question',
+    hideInForm: true,
+    hideInSearch: true,
+    hideInSetting: true,
+    render: (_, row: API.GiftQuestion) => {
+      const obj = parse(row.value);
+
+      if (obj?.[0] && 'stem' in obj[0]) {
+        return obj[0].stem.text;
+      }
+      return 'question';
+    },
+  },
+];
+
+interface OnSortEndParams {
+  oldIndex: number;
+  newIndex: number;
+}
+
+interface Props {
   quizId: number;
   questions: API.GiftQuestion[];
   onAdded?: () => void;
   onRemoved?: () => void;
   onEdited?: () => void;
-}> = ({ questions, onAdded, onRemoved, onEdited, quizId }) => {
+  tableHeader?: React.ReactNode;
+  tableLoading?: boolean;
+}
+
+export const Table: React.FC<Props> = ({
+  questions,
+  onAdded,
+  onRemoved,
+  onEdited,
+  quizId,
+  tableLoading,
+  tableHeader,
+}) => {
   const [editQuestion, setEditQuestion] = useState<API.GiftQuestion>();
   const [loading, setLoading] = useState(false);
   const [showNew, setShowNew] = useState(false);
@@ -42,10 +98,7 @@ export const Table: React.FC<{
     setDataSource(questions);
   }, [questions]);
 
-  const SortableItem = SortableElement((props: any) => <tr {...props} />);
-  const SortContainer = SortableContainer((props: any) => <tbody {...props} />);
-
-  const onSortEnd = ({ oldIndex, newIndex }: { oldIndex: number; newIndex: number }) => {
+  const onSortEnd = ({ oldIndex, newIndex }: OnSortEndParams) => {
     if (oldIndex !== newIndex) {
       const newData = arrayMoveImmutable(dataSource.slice(), oldIndex, newIndex)
         .filter(Boolean)
@@ -61,6 +114,7 @@ export const Table: React.FC<{
     }
   };
 
+  // TODO types & refactor https://react.dev/learn/your-first-component#nesting-and-organizing-components
   const DraggableContainer = (props: any) => (
     <SortContainer
       useDragHandle
@@ -71,6 +125,7 @@ export const Table: React.FC<{
     />
   );
 
+  // TODO types & refactor https://react.dev/learn/your-first-component#nesting-and-organizing-components
   const DraggableBodyRow = (props: any) => {
     const { className, style, ...restProps } = props;
     // function findIndex base on Table rowKey props and should always be a right array index
@@ -99,7 +154,7 @@ export const Table: React.FC<{
   }, [newQuestion, quizId]);
 
   return (
-    <Fragment>
+    <>
       <Drawer open={!!editQuestion} onClose={() => setEditQuestion(undefined)}>
         {editQuestion && (
           <EditQuestion
@@ -123,7 +178,6 @@ export const Table: React.FC<{
           />
         )}
       </Drawer>
-
       <Drawer open={showNew} onClose={() => setShowNew(false)}>
         <GiftQuizNewQuestionEditor
           debug={debug}
@@ -132,91 +186,61 @@ export const Table: React.FC<{
           onChange={(value) => setNewQuestion(value)}
         />
       </Drawer>
-
-      <Space
-        align="end"
-        direction="horizontal"
-        style={{ display: 'flex', marginBottom: '12px', justifyContent: 'end' }}
-      >
-        <Button icon={<PlusCircleOutlined />} type="primary" onClick={() => setShowNew(true)}>
-          {' '}
-          <FormattedMessage id="addNewQuestion" defaultMessage="Add new question" />
-        </Button>
-      </Space>
-
       <ProTable
         search={false}
-        toolBarRender={false}
+        cardProps={{ bodyStyle: { padding: 0 } }}
+        loading={tableLoading}
+        headerTitle={tableHeader}
+        toolbar={{ settings: [] }}
+        toolBarRender={() => [
+          <Button
+            key="add"
+            icon={<PlusCircleOutlined />}
+            type="primary"
+            onClick={() => setShowNew(true)}
+          >
+            {' '}
+            <FormattedMessage id="addNewQuestion" defaultMessage="Add new question" />
+          </Button>,
+        ]}
         columns={[
-          {
-            title: <FormattedMessage id="sort" defaultMessage="sort" />,
-            dataIndex: 'id',
-            width: 60,
-            className: 'drag-visible',
-            render: () => <DragHandle />,
-          },
-          {
-            title: <FormattedMessage id="type" defaultMessage="Type" />,
-            dataIndex: 'type',
-            hideInForm: true,
-            hideInSearch: true,
-            hideInSetting: true,
-            render: (_, row: API.GiftQuestion) => <FormattedMessage id={row.type} />,
-          },
-          {
-            title: <FormattedMessage id="question" defaultMessage="Question" />,
-            dataIndex: 'question',
-            hideInForm: true,
-            hideInSearch: true,
-            hideInSetting: true,
-            render: (_, row: API.GiftQuestion) => {
-              const obj = parse(row.value);
-
-              if (obj && obj[0] && 'stem' in obj[0]) {
-                return obj[0].stem.text;
-              }
-              return 'question';
-            },
-          },
+          ...staticColumns,
           {
             title: <FormattedMessage id="actions" defaultMessage="Actions" />,
             dataIndex: 'address',
-            hideInForm: true,
-            hideInSearch: true,
-            hideInSetting: true,
-            render: (_, row) => (
-              <Space>
-                <Button
-                  size="small"
-                  type="primary"
-                  onClick={() => {
-                    setEditQuestion(row);
-                  }}
-                >
-                  <FormattedMessage id="Questions.edit" defaultMessage="Edit Question" />
-                </Button>
-                <Button
-                  loading={loading}
-                  key={'delete'}
-                  size="small"
-                  danger
-                  onClick={() => {
-                    setLoading(true);
-                    deleteQuestion(row.id)
-                      .then(() => {
-                        if (onRemoved) {
-                          onRemoved();
-                        }
-                      })
-                      .finally(() => {
-                        setLoading(false);
-                      });
-                  }}
-                >
-                  <FormattedMessage id="Questions.delete" defaultMessage="Delete Question" />
-                </Button>
-              </Space>
-            ),
+            valueType: 'option',
+            render: (_, row) => [
+              <Button
+                key="edit"
+                size="small"
+                type="primary"
+                onClick={() => {
+                  setEditQuestion(row);
+                }}
+              >
+                <FormattedMessage id="Questions.edit" defaultMessage="Edit Question" />
+              </Button>,
+              <Button
+                loading={loading}
+                key="delete"
+                size="small"
+                danger
+                onClick={() => {
+                  setLoading(true);
+                  deleteQuestion(row.id)
+                    .then(() => {
+                      if (onRemoved) {
+                        onRemoved();
+                      }
+                    })
+                    .finally(() => {
+                      setLoading(false);
+                    });
+                }}
+              >
+                <FormattedMessage id="Questions.delete" defaultMessage="Delete Question" />
+              </Button>,
+            ],
           },
         ]}
         rowKey={(record) => record.id}
@@ -232,6 +256,6 @@ export const Table: React.FC<{
       <Typography style={{ marginTop: '3em' }}>
         Debug GIFT: <Switch checked={debug} onChange={(v) => setDebug(v)} />
       </Typography>
-    </Fragment>
+    </>
   );
 };
