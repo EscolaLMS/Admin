e55e6d868e619694d9d9227fcff56632f29005b3
Szymon Dolnik <szymon2407@hotmail.com>
2023-07-17 15:50:40 +0200

    TrueFalse editor refactor

---
+++ src/components/GiftQuizQuestions/editor/question.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { QuestionType } from '@/services/escola-lms/enums';
+import { GiftQuizQuestionMultipleChoiceEditor } from './multiplechoice';
+import { GiftQuizQuestionTrueFalseEditor } from './true_false';
+
+interface Props {
+  type: QuestionType | undefined;
+}
+
+export const GiftQuizQuestion: React.FC<Props> = ({ type }) => {
+  if (!type) return null;
+
+  switch (type) {
+    case QuestionType.MULTIPLE_CHOICE:
+    case QuestionType.MULTIPLE_CHOICE_WITH_MULTIPLE_RIGHT_ANSWERS:
+      return <GiftQuizQuestionMultipleChoiceEditor type={type} />;
+    case QuestionType.TRUE_FALSE: {
+      return <GiftQuizQuestionTrueFalseEditor />;
+    }
+    case QuestionType.SHORT_ANSWERS: {
+      return <pre>{type}</pre>;
+    }
+    case QuestionType.MATCHING: {
+      return <pre>{type}</pre>;
+    }
+    case QuestionType.NUMERICAL_QUESTION: {
+      return <pre>{type}</pre>;
+    }
+    case QuestionType.ESSAY: {
+      return <pre>{type}</pre>;
+    }
+    case QuestionType.DESCRIPTION: {
+      return <pre>{type}</pre>;
+    }
+  }
+
+  return <pre>Unsupported type {type}</pre>;
+};
