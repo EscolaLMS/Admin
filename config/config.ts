72be03fd5fac6a8582bce7a19cf670be355b1f01
Mateusz Qunabu <mateusz@qunabu.com>
2023-07-25 12:13:03 +0100

    sentry source maps WELLMS-371 (#940)
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>

--- config/config.ts
+++ config/config.ts
@@ -50,6 +50,7 @@ export default defineConfig({
   title: false,
   ignoreMomentLocale: true,
   proxy: proxy[REACT_APP_ENV || 'dev'],
+  devtool: 'source-map',
   manifest: {
     basePath: '/',
   },
