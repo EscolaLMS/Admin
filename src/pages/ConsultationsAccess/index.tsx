6edf55fb9ba44cac2e787a497065a28fc5eeee7f
Szymon Dolnik <szymon2407@hotmail.com>
2023-08-09 11:27:52 +0200

    ConsultationAccess refactor

--- src/pages/ConsultationsAccess/index.tsx
+++ src/pages/ConsultationsAccess/index.tsx
@@ -1,18 +1,17 @@
-import { Button, Tag, Space } from 'antd';
-import React, { useRef, useState } from 'react';
-import { useIntl, FormattedMessage } from 'umi';
-import { PageContainer } from '@ant-design/pro-layout';
-import type { ProColumns, ActionType } from '@ant-design/pro-table';
-import ProTable from '@ant-design/pro-table';
-import UserSelect from '@/components/UserSelect';
+import React, { useMemo, useRef, useState } from 'react';
 import { format } from 'date-fns';
-import { DATETIME_FORMAT } from '@/consts/dates';
-import { ApproveForm } from './approveForm';
-import { DisapproveForm } from './disapproveForm';
+import { useIntl, FormattedMessage } from 'umi';
+import { Button, Tag, Space } from 'antd';
+import { PageContainer } from '@ant-design/pro-layout';
+import ProTable, { type ProColumns, type ActionType } from '@ant-design/pro-table';
 
-import { consultationAccess } from '@/services/escola-lms/consultations_access';
+import UserSelect from '@/components/UserSelect';
 import TypeButtonDrawer from '@/components/TypeButtonDrawer';
 import { CollectionSelect } from '@/components/CollectionSelect';
+import { DATETIME_FORMAT } from '@/consts/dates';
+import { consultationAccess } from '@/services/escola-lms/consultations_access';
+import { ApproveForm } from './approveForm';
+import { DisapproveForm } from './disapproveForm';
 
 const TableList: React.FC = () => {
   const actionRef = useRef<ActionType>();
@@ -22,176 +21,180 @@ const TableList: React.FC = () => {
   const [approveTerm, setApproveTerm] =
     useState<EscolaLms.ConsultationAccess.Models.ConsultationAccessEnquiryProposedTerm>();
 
-  const columns: ProColumns<API.ConsultationAccessEnquiryListItem>[] = [
-    {
-      title: <FormattedMessage id="ID" defaultMessage="ID" />,
-      dataIndex: 'id',
-      hideInSearch: true,
-      sorter: true,
-      width: '80px',
-    },
-    {
-      title: <FormattedMessage id="description" defaultMessage="Description" />,
-      dataIndex: 'description',
-      hideInSearch: true,
-      sorter: true,
-    },
-    {
-      title: <FormattedMessage id="user" defaultMessage="User" />,
-      dataIndex: 'user_id',
-      renderFormItem: (item, { type, defaultRender, ...rest }, form) => {
-        if (type === 'form') {
-          return null;
-        }
-        const stateType = form.getFieldValue('state');
-        return (
-          <UserSelect
-            {...rest}
-            state={{
-              type: stateType,
-            }}
-          />
-        );
+  const columns: ProColumns<API.ConsultationAccessEnquiryListItem>[] = useMemo(
+    () => [
+      {
+        title: <FormattedMessage id="ID" defaultMessage="ID" />,
+        dataIndex: 'id',
+        hideInSearch: true,
+        sorter: true,
+        width: '80px',
       },
-      render: (_, record) =>
-        record.user?.id && (
-          <TypeButtonDrawer
-            key={'user'}
-            type="EscolaLms\Core\Models\User"
-            type_id={record.user?.id}
-          >
-            <Tag>{record.user.email}</Tag>
-          </TypeButtonDrawer>
-        ),
-    },
-
-    {
-      title: <FormattedMessage id="consultation_id" defaultMessage="Consultation" />,
-      dataIndex: 'consultation_id',
-
-      renderFormItem: (item, { type, defaultRender, ...rest }, form) => {
-        if (type === 'form') {
-          return null;
-        }
-        const stateType = form.getFieldValue('state');
-        return (
-          <CollectionSelect
-            modelType="CONSULTATIONS"
-            {...rest}
-            state={{
-              type: stateType,
-            }}
-          />
-        );
+      {
+        title: <FormattedMessage id="description" defaultMessage="Description" />,
+        dataIndex: 'description',
+        hideInSearch: true,
+        sorter: true,
       },
-
-      render: (_, record) =>
-        record.consultation?.id && (
-          <TypeButtonDrawer
-            key={'consultation_id'}
-            type="EscolaLms\Consultations\Models\Consultation"
-            type_id={record.consultation?.id}
-          >
-            <Tag>{record.consultation.name}</Tag>
-          </TypeButtonDrawer>
-        ),
-    },
-    {
-      title: <FormattedMessage id="consultation_term_date" defaultMessage="Term Date" />,
-      dataIndex: 'consultation_term',
-      hideInSearch: true,
-      sorter: true,
-      render: (_, record) => {
-        return record.consultation_term ? (
-          <>
-            <FormattedMessage id="approved_date" defaultMessage="Approved Date" />:
-            {format(new Date(record.consultation_term.date), DATETIME_FORMAT)}
-          </>
-        ) : (
-          <Space direction="vertical">
-            {record.proposed_terms.map((term) => (
-              <Button
-                key={term.id}
-                size="small"
-                type="primary"
-                onClick={() => {
-                  setApproveTerm(term);
-                }}
-              >
-                <FormattedMessage id="approve" defaultMessage="Approve Term" />
-                {':'}
-                {format(new Date(term.proposed_at), DATETIME_FORMAT)}
-              </Button>
-            ))}
-          </Space>
-        );
-      },
-    },
-    {
-      title: <FormattedMessage id="meeting_link" defaultMessage="Meeting Link" />,
-      dataIndex: 'meeting_link',
-      hideInSearch: true,
-      render: (_, record) =>
-        record.meeting_link ? (
-          <Button target="_blank" type="primary" href={record.meeting_link}>
-            {new URL(record.meeting_link).host}
-          </Button>
-        ) : (
-          <React.Fragment />
-        ),
-    },
-    {
-      title: <FormattedMessage id="created_at" defaultMessage="Created at" />,
-      dataIndex: 'created_at',
-      hideInSearch: true,
-      sorter: true,
-      render: (_, record) =>
-        record.created_at && format(new Date(record.created_at), DATETIME_FORMAT),
-    },
-    {
-      title: <FormattedMessage id="status" defaultMessage="Status" />,
-      dataIndex: 'status',
-      sorter: true,
-      valueType: 'select',
-      valueEnum: {
-        pending: {
-          text: 'pending',
-          status: 'pending',
+      {
+        title: <FormattedMessage id="user" defaultMessage="User" />,
+        dataIndex: 'user_id',
+        renderFormItem: (_i, { type, defaultRender, ...rest }, form) => {
+          if (type === 'form') {
+            return null;
+          }
+          const stateType = form.getFieldValue('state');
+          return (
+            <UserSelect
+              {...rest}
+              state={{
+                type: stateType,
+              }}
+            />
+          );
         },
-        approved: {
-          text: 'approved',
-          status: 'approved',
+        render: (_, record) =>
+          record.user?.id && (
+            <TypeButtonDrawer
+              key={'user'}
+              type="EscolaLms\Core\Models\User"
+              type_id={record.user?.id}
+            >
+              <Tag>{record.user.email}</Tag>
+            </TypeButtonDrawer>
+          ),
+      },
+
+      {
+        title: <FormattedMessage id="consultation_id" defaultMessage="Consultation" />,
+        dataIndex: 'consultation_id',
+
+        renderFormItem: (_i, { type, defaultRender, ...rest }, form) => {
+          if (type === 'form') {
+            return null;
+          }
+          const stateType = form.getFieldValue('state');
+          return (
+            <CollectionSelect
+              modelType="CONSULTATIONS"
+              {...rest}
+              state={{
+                type: stateType,
+              }}
+            />
+          );
+        },
+
+        render: (_, record) =>
+          record.consultation?.id && (
+            <TypeButtonDrawer
+              key={'consultation_id'}
+              type="EscolaLms\Consultations\Models\Consultation"
+              type_id={record.consultation?.id}
+            >
+              <Tag>{record.consultation.name}</Tag>
+            </TypeButtonDrawer>
+          ),
+      },
+      {
+        title: <FormattedMessage id="consultation_term_date" defaultMessage="Term Date" />,
+        dataIndex: 'consultation_term',
+        hideInSearch: true,
+        sorter: true,
+        render: (_, record) => {
+          return record.consultation_term ? (
+            <>
+              <FormattedMessage id="approved_date" defaultMessage="Approved Date" />:
+              {format(new Date(record.consultation_term.date), DATETIME_FORMAT)}
+            </>
+          ) : (
+            <Space direction="vertical">
+              {record.proposed_terms.map((term) => (
+                <Button
+                  key={term.id}
+                  size="small"
+                  type="primary"
+                  onClick={() => {
+                    setApproveTerm(term);
+                  }}
+                >
+                  <FormattedMessage id="approve" defaultMessage="Approve Term" />
+                  {':'}
+                  {format(new Date(term.proposed_at), DATETIME_FORMAT)}
+                </Button>
+              ))}
+            </Space>
+          );
         },
       },
-      render: (_, record) =>
-        record.status === 'pending' ? (
-          <Button
-            danger
-            size="small"
-            type="primary"
-            onClick={() => {
-              setDisapproveId(record.id);
-            }}
-          >
-            <FormattedMessage id="disapprove" defaultMessage="Disapprove" />
-          </Button>
-        ) : (
-          record.status
-        ),
-    },
-  ];
+
+      {
+        title: <FormattedMessage id="meeting_link" defaultMessage="Meeting Link" />,
+        dataIndex: 'meeting_link',
+        hideInSearch: true,
+        render: (_, record) =>
+          record.meeting_link ? (
+            <Button target="_blank" type="primary" href={record.meeting_link}>
+              {new URL(record.meeting_link).host}
+            </Button>
+          ) : (
+            <React.Fragment />
+          ),
+      },
+      {
+        title: <FormattedMessage id="created_at" defaultMessage="Created at" />,
+        dataIndex: 'created_at',
+        hideInSearch: true,
+        sorter: true,
+        render: (_, record) =>
+          record.created_at && format(new Date(record.created_at), DATETIME_FORMAT),
+      },
+      {
+        title: <FormattedMessage id="status" defaultMessage="Status" />,
+        dataIndex: 'status',
+        sorter: true,
+        valueType: 'select',
+        valueEnum: {
+          pending: {
+            text: 'pending',
+            status: 'pending',
+          },
+          approved: {
+            text: 'approved',
+            status: 'approved',
+          },
+        },
+        render: (_, record) =>
+          record.status === 'pending' ? (
+            <Button
+              danger
+              size="small"
+              type="primary"
+              onClick={() => {
+                setDisapproveId(record.id);
+              }}
+            >
+              <FormattedMessage id="disapprove" defaultMessage="Disapprove" />
+            </Button>
+          ) : (
+            record.status
+          ),
+      },
+    ],
+    [],
+  );
 
   return (
     <PageContainer>
       <ApproveForm
         term={approveTerm}
         onClose={() => setApproveTerm(undefined)}
-        onSuccess={() => actionRef && actionRef.current?.reload()}
+        onSuccess={() => actionRef.current?.reload()}
       />
       <DisapproveForm
         id={disapproveId}
         onClose={() => setDisapproveId(undefined)}
-        onSuccess={() => actionRef && actionRef.current?.reload()}
+        onSuccess={() => actionRef.current?.reload()}
       />
 
       <ProTable<API.ConsultationAccessEnquiryListItem, API.ConsultationAccessEnquiryListParams>
@@ -204,7 +207,7 @@ const TableList: React.FC = () => {
         }}
         actionRef={actionRef}
         rowKey="id"
-        request={(
+        request={async (
           {
             pageSize,
             current,
@@ -218,7 +221,8 @@ const TableList: React.FC = () => {
           sort,
         ) => {
           const sortArr = sort && Object.entries(sort)[0];
-          return consultationAccess({
+
+          const response = await consultationAccess({
             pageSize,
             current,
             consultation_id,
@@ -229,16 +233,17 @@ const TableList: React.FC = () => {
             proposed_at_to,
             order_by: sortArr && sortArr[0],
             order: sortArr ? (sortArr[1] === 'ascend' ? 'ASC' : 'DESC') : undefined,
-          }).then((response) => {
-            if (response.success) {
-              return {
-                data: response.data,
-                total: response.meta.total,
-                success: true,
-              };
-            }
-            return [];
           });
+
+          if (!response.success) {
+            return { data: [], total: 0, success: false };
+          }
+
+          return {
+            data: response.data,
+            total: response.meta.total,
+            success: true,
+          };
         }}
         columns={columns}
       />
