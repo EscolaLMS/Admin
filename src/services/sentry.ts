6e98f41e716167e6f7fa1193d17649775d0e9764
Mateusz Wojczal <mateusz@wojczal.com>
2024-11-18 16:24:53 +0100

    sentry update

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
 
 declare const REACT_APP_SENTRYDSN: string;
 declare const REACT_APP_SENTRY_RELEASE: string;
@@ -17,8 +16,14 @@ function configSentry() {
   ) {
     Sentry.init({
       dsn: window.REACT_APP_SENTRYDSN || REACT_APP_SENTRYDSN,
-      integrations: [new BrowserTracing()],
-      tracesSampleRate: 1,
+      integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+      // Tracing
+      tracesSampleRate: 0.1, //  Capture 100% of the transactions
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+      // Session Replay
+      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
       release: `admin-${REACT_APP_SENTRY_RELEASE || ''}`,
     });
   }
