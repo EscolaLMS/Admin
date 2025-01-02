99dc38bd9308144a2bc9980b1c1b9421828ae32d
Mateusz Wojczal <mateusz@wojczal.com>
2025-01-02 22:07:23 +0100

    sentry

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -27,10 +27,7 @@ function configSentry() {
       replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
       replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
       environment: window.REACT_APP_SENTRY_ENV || REACT_APP_SENTRY_ENV || 'development',
-      release:
-        window.REACT_APP_SENTRY_RELEASE || REACT_APP_SENTRY_RELEASE
-          ? `admin@${window.REACT_APP_SENTRY_RELEASE || REACT_APP_SENTRY_RELEASE}`
-          : 'admin@dev-main',
+      release: window.REACT_APP_SENTRY_RELEASE || REACT_APP_SENTRY_RELEASE || 'dev-main',
     });
   }
   return null;
